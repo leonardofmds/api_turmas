a4188f597484b1d9d07e2a7e844aa3f2d2635019
SG LEONARDO MENEZES <leonardo.felipe@marinha.mil.br>
2025-03-10 14:36:24 -0300

    first commit

---
+++ src/test/java/br/com/coti/api_turmas/ApiTurmasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.coti.api_turmas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTurmasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
